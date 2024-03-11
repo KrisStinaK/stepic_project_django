c90c94c8903b1d65606ce24b177141e2c6d4808c
KrisStinaK <112583110+KrisStinaK@users.noreply.github.com>
2024-03-11 18:22:44 +0300

    Добавлено восстановление пароля через почту

--- site_django/site_django/settings.py
+++ site_django/site_django/settings.py
@@ -148,6 +148,19 @@ AUTHENTICATION_BACKENDS = [
     'users.authentication.EmailAuthBackend',
 ]
 
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+# EMAIL_HOST = 'smtp.yandex.ru'
+# EMAIL_PORT = 465
+# EMAIL_USE_SSL = True
+# EMAIL_HOST_USER = ''
+# EMAIL_HOST_PASSWORD = ''
+#
+# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+# SERVER_EMAIL = EMAIL_HOST_USER
+# EMAIL_ADMIN = EMAIL_HOST_USER
+
 
 import mimetypes
 mimetypes.add_type("application/javascript", ".js", True)
